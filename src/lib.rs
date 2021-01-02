4f1bc4e2187528ae60a520f5c910cc7199f90ae6
ank3r <mwangishadrack5@gmail.com>
2021-01-02 19:06:25 +0300

    add network behaviours

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,4 @@
-use libp2p::{
-    floodsub::{Topic, Floodsub, FloodsubEvent},
-    PeerId,
-    identity,
-    swarm::{NetworkBehaviourEventProcess, SwarmBuilder, Swarm},
-    mdns::{Mdns, TokioMdns, MdnsEvent},
-    NetworkBehaviour, Transport, 
-    noise::{Keypair, X25519Spec, NoiseConfig},
-    tcp::TokioTcpConfig,
-    core::upgrade,
-    yamux::YamuxConfig,
-    Multiaddr
-};
+use libp2p::{Multiaddr, NetworkBehaviour, PeerId, Transport, core::upgrade, floodsub::{Floodsub, FloodsubConfig, FloodsubEvent, Topic}, identity, mdns::{Mdns, TokioMdns, MdnsEvent}, noise::{Keypair, X25519Spec, NoiseConfig}, swarm::{NetworkBehaviourEventProcess, SwarmBuilder, Swarm}, tcp::TokioTcpConfig, yamux::YamuxConfig};
 
 use tokio::{sync::mpsc};
 use log::{info, error};
@@ -32,13 +20,13 @@ pub struct Message {
 
 impl Message {
     // Create a new message
-    pub fn new(msg: String, source: String, topic: String) -> Self {
+    pub fn new(msg: &str, source: PeerId, topic: &str) -> Self {
         Self {
             id: Uuid::new_v4(),
-            msg,
-            source,
+            msg: msg.to_string(),
+            source: source.to_string(),
             ts: SystemTime::now(),
-            topic
+            topic: topic.to_string()
         }
     }
 }
@@ -57,9 +45,12 @@ struct ChatBehavior {
 impl NetworkBehaviourEventProcess<FloodsubEvent> for ChatBehavior {
     fn inject_event(&mut self, event: FloodsubEvent) {
         match event {
-            _ => {
-                info!("New event")
-            }
+            FloodsubEvent::Message(msg) => {
+                if let Ok(resp) = serde_json::from_slice::<Message>(&msg.data){
+                    info!("Message from {} - {}", msg.source, resp.msg);
+                }
+            },
+            _ => ()
         }
     }
 }
@@ -68,9 +59,19 @@ impl NetworkBehaviourEventProcess<FloodsubEvent> for ChatBehavior {
 impl NetworkBehaviourEventProcess<MdnsEvent> for ChatBehavior {
     fn inject_event(&mut self, event: MdnsEvent) {
         match event {
-            _ => {
-                info!("Mdns event")
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _) in expired_list{
+                    if !self.mdns.has_node(&peer){
+                        self.floodsub.remove_node_from_partial_view(&peer)
+                    }
+                }
+            },
+            MdnsEvent::Discovered(discovered) => {
+                for (peer,_) in discovered{
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
             }
+            _ => ()
         }
     }
 }
@@ -79,12 +80,11 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for ChatBehavior {
 pub struct Peer {
     topics: Vec<Topic>,
     peer_id: PeerId,
-    keys: identity::Keypair,
     pub name: String,
-    messages: Vec<Message>,
+    pub messages: Vec<Message>,
     swarm: Swarm<ChatBehavior>,
     addr: Multiaddr,
-    pub receiver: mpsc::UnboundedReceiver<Message>
+    pub receiver: mpsc::UnboundedReceiver<Message>,
 }
 
 
@@ -96,7 +96,7 @@ impl Peer {
         let peer_id = PeerId::from(keys.public());
 
         // create a stream channel 
-        let (sender, mut receiver) = mpsc::unbounded_channel();
+        let (sender, receiver) = mpsc::unbounded_channel();
 
         let auth_keys = Keypair::<X25519Spec>::new()
             .into_authentic(&keys)
@@ -113,13 +113,13 @@ impl Peer {
             .boxed();
 
         let behavior = ChatBehavior {
-            floodsub: Floodsub::new(peer_id.clone()),
+            floodsub: Floodsub::from_config(FloodsubConfig{local_peer_id: peer_id.clone(), subscribe_local_messages: true}),
             mdns: TokioMdns::new().expect("Can create an mdns"),
             sender
         };
 
         // create a swarm
-        let mut swarm = SwarmBuilder::new(transport, behavior, peer_id.clone())
+        let swarm = SwarmBuilder::new(transport, behavior, peer_id.clone())
             .executor(Box::new(|fut|{
                 tokio::spawn(fut);
             })).build();
@@ -128,7 +128,6 @@ impl Peer {
 
         Self {
             topics: vec![],
-            keys,
             peer_id,
             name: name.to_string(),
             messages: vec![],
@@ -139,8 +138,13 @@ impl Peer {
     }
 
     /// Create a new topic and subscribe it to the network
-    pub fn join_topic(&mut self, topic_name: String) {
+    pub fn join_topic(&mut self, topic_name: &str) {
         let topic = Topic::new(topic_name);
+
+        // if topic exists just exit 
+        if self.topics.contains(&topic){
+            return;
+        }
 
         // subscribe it to the network 
         if self.swarm.floodsub.subscribe(topic.clone()) /*Insert subscription code*/{
@@ -158,6 +162,8 @@ impl Peer {
     /// connect to the network
     /// it assumes caller is an async function 
     pub fn connect(&mut self) {
+        info!("Connecting to the network!");
+
         Swarm::listen_on(
             &mut self.swarm, self.addr.clone()
         ).expect("swarm can be started");
@@ -168,6 +174,16 @@ impl Peer {
         self.peer_id.clone()
     }
 
+    /// send a message 
+    pub fn send_message(&mut self, topic: &str, message: &str){
+        // find the topic in the subscriptions
+        let message = Message::new(message, self.get_id(), topic);
+        let ser_message = serde_json::to_string(&message).expect("can jsonify message");
+        
+        self.swarm.floodsub.publish(Topic::new(topic), ser_message);
 
+        self.messages.push(message);
+        info!("Message sent");
+    }
 
 }
