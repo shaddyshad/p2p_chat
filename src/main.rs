16e7e8d239835a413ef20cdd30173f8a69661801
ank3r <mwangishadrack5@gmail.com>
2021-01-02 18:14:50 +0300

    add a few input evt handlers

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             match event {
                 EvtType::Input(line) => match line.as_str(){
                     "q" => break,
+                    cmd if line.starts_with("create t") => handle_create_topic(cmd, &mut peer),
+                    _ if line.starts_with("ls t") => handle_list_topics(&peer),
+                    cmd if line.starts_with("join t") => handle_join_topic(cmd, &mut peer),
                     _ => error!("unknown command")
                 },
-                EvtType::Response(resp) => {
+                EvtType::Response(_resp) => {
                     info!("New message ")
                 }
             }
@@ -44,4 +47,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 
+}
+
+/// create a new topic 
+fn handle_create_topic(cmd: &str, peer: &mut Peer){
+    let rest = cmd.strip_prefix("create t").expect("no topic found");
+
+    peer.join_topic(rest);
+    info!("new topic {} added!", rest);
+}
+
+/// list subscribed topics 
+fn handle_list_topics(peer: &Peer){
+    let topics = peer.get_topics();
+
+    topics.iter().for_each(|t| info!("Topic: {}", t));
+}
+
+// join a topic 
+fn handle_join_topic(cmd: &str, peer: &mut Peer){
+    let rest = cmd.strip_prefix("join t").expect("no topic found");
+
+    peer.join_topic(rest);
+
+    info!("joined topic {} added!", rest);
 }
