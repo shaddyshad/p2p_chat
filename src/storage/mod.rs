f171d76730d7fb1fc6d42ea81e83e5f915429aa9
ank3r <mwangishadrack5@gmail.com>
2021-01-03 17:44:28 +0300

    remove associated types in storage

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,12 +1,9 @@
-#[allow(dead_code)]
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
+use super::Result;
+pub mod memory_storage;
 /// A trait defining types that can provide predicate capabilities 
 /// for storage queries 
-pub trait QueryPredicate {
-    type Item ;
-
-    fn matches(&self, other: &Self::Item) -> bool;    
+pub trait QueryPredicate<T> {
+    fn matches(&self, other: &T) -> bool;    
 }
 
 /// A trait for types that provide storage capabilities. Examples would include  
@@ -24,17 +21,22 @@ pub trait Storage {
     fn save(&mut self, item: Self::Item) -> Result<()>;
 
     /// Retieve all records in the storage 
-    fn list(&self) -> Box<dyn Iterator<Item=Self::Item>>;
+    fn list(&self) -> Vec<Self::Item>;
 
     /// Delete all records 
     /// Returns a count of all deleted records or an error if any occured
     fn remove_all(&mut self) -> Result<usize>;
 
     /// Predicated version of list 
-    fn find<Q: QueryPredicate>(&self, query: Q) -> Box<dyn Iterator<Item=Self::Item>>;
+    fn find<Q: QueryPredicate<Self::Item>>(&self, query: Q) -> Vec<Self::Item>;
 
     /// Predicated remove 
     /// Returns a Result with count of the removed elements or an error 
-    fn remove_if<Q:QueryPredicate>(&mut self, query: Q) -> Result<usize>;
+    fn remove_if<Q:QueryPredicate<Self::Item>>(&mut self, query: Q) -> Result<usize>{
+        Ok(0)
+    }
+
 
 }
+
+
