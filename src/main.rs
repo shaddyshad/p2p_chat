55f1c3fe54553a64cacd3d3e4a71cb3ef650433b
ank3r <mwangishadrack5@gmail.com>
2021-01-03 18:23:07 +0300

    test on a dummy subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,42 @@
 extern crate peer_chat;
-use log::{info, error};
-use peer_chat::{Peer, Message};
-use tokio::{io::{stdin, BufReader, AsyncBufReadExt}};
+use log::{info};
 
-enum EvtType {
-    Input(String),
-    Response(Message)
+
+use peer_chat::{Peer, storage::MemoryStorage, Subscriptions, Group};
+
+// stub subscriptions 
+struct DummySubscriptions;
+
+impl Subscriptions for DummySubscriptions {
+    fn subscribe(&mut self, peer_id: &str, topic: &str) -> bool {
+        info!("{} subscribed to {}", peer_id, topic);
+
+        true 
+    }
+
+    fn unsubscribe(&mut self, peer_id: &str, topic: &str) -> bool {
+        info!("{} unsubscribed from {}", peer_id, topic);
+
+        true 
+    }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     pretty_env_logger::init();
-    info!("Welcome to peer chat. ");
-    let mut name = "shaddy shad".to_string();
+    // create two a peer 
+    let peer =  Peer::new("asdhsdf".to_string(), "ank3r".to_string());
 
-    if let Some(n) = std::env::args().nth(1){
-        name = n;
-    }
+    // get the sub and memory storage 
+    let subscriptions = DummySubscriptions;
+    let storage: MemoryStorage<Group> = MemoryStorage::new();
 
-    let mut peer = Peer::new(&name);
-    peer.connect();
-    peer.conn_status();
+    // create a new group 
+    let mut new_group = peer.new_group("chat001".to_string(), storage, subscriptions);
 
+    // save the group by subscribing 
+    new_group.subscribe();
 
-    if let Some(to_dial) = std::env::args().nth(2){
-        info!("Dialed {:?}", to_dial);
-        peer.dial(to_dial);
-    }
-
+    // finally, unsubscribe 
+    new_group.unsubscribe();
     
-    info!("PeerId: {}", peer.get_id());
-    info!("Peer name: {}", peer.name);
-
-    let mut stdin = BufReader::new(stdin()).lines();
-
-    
-
-    loop {
-        let evt = tokio::select! {
-            line = stdin.next_line() => Some(EvtType::Input(line.expect("can get a line ").expect("can read a line from stdin"))),
-            response = peer.receiver.recv() => Some(EvtType::Response(response.expect("an event exists")))
-        };
-
-        if let Some(event) = evt {
-            match event {
-                EvtType::Input(line) => match line.as_str(){
-                    "q" => break,
-                    cmd if line.starts_with("create t") => handle_create_topic(cmd, &mut peer),
-                    _ if line.starts_with("ls t") => handle_list_topics(&peer),
-                    _ if line.starts_with("ls m") => handle_list_messages(&peer),
-                    cmd if line.starts_with("join t") => handle_join_topic(cmd, &mut peer),
-                    cmd if line.starts_with("create m") => handle_create_message(cmd, &mut peer),
-                    _ => error!("unknown command")
-                },
-                EvtType::Response(_resp) => {
-                    info!("New message ")
-                }
-            }
-        } 
-    }
-
-    Ok(())
-
-}
-
-/// create a new topic 
-fn handle_create_topic(cmd: &str, peer: &mut Peer){
-    let rest = cmd.strip_prefix("create t").expect("no topic found");
-
-    peer.join_topic(rest);
-    info!("new topic {} added!", rest);
-}
-
-/// list subscribed topics 
-fn handle_list_topics(peer: &Peer){
-    let topics = peer.get_topics();
-
-    topics.iter().for_each(|t| info!("Topic: {}", t));
-}
-
-// join a topic 
-fn handle_join_topic(cmd: &str, peer: &mut Peer){
-    let rest = cmd.strip_prefix("join t").expect("no topic found");
-
-    peer.join_topic(rest);
-
-    info!("joined topic {} added!", rest);
-}
-
-fn handle_list_messages(peer: &Peer){
-    peer.messages
-        .iter()
-        .for_each(|msg| info!("{:?}", msg));
-}
-
-/// Create a new message 
-fn handle_create_message(cmd: &str, peer: &mut Peer){
-    let rest = cmd.strip_prefix("create m ").expect("invalid command");
-    let elements: Vec<&str> = rest.split(" ").collect();
-
-    if elements.len() < 2 {
-        info!("Too few arguments - format topic|message");
-    }else{
-        let topic = elements.get(0).expect("can get a topic");
-        let message = &elements[1..].join(" ");
-
-        peer.send_message(topic, message);
-    }
 }
