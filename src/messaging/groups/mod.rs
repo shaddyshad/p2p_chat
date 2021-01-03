08223a35252b4b9970bb92ea4d9feda69d161717
ank3r <mwangishadrack5@gmail.com>
2021-01-03 16:41:27 +0300

    add new group facility

---
+++ src/messaging/groups/mod.rs
@@ -0,0 +1,22 @@
+use uuid::Uuid;
+mod new_group;
+use std::time::SystemTime;
+
+/// Representation of a group chat 
+pub struct Group {
+    name: String,
+    id: Uuid,
+    creator: String,
+    ts: SystemTime
+}
+
+impl Group {
+    pub fn new(name: String, creator: String) -> Self {
+        Self {
+            name,
+            creator,
+            id: Uuid::new_v4(),
+            ts: SystemTime::now()
+        }
+    }
+}
---
+++ src/messaging/groups/new_group.rs
@@ -0,0 +1,56 @@
+use super::Group;
+use crate::{storage::{Storage, QueryPredicate}, Result};
+use super::super::subscriptions::Subscriptions;
+
+/// Query to check for matches in group name 
+struct GroupNameQuery<'a>(&'a str);
+
+impl<'a> QueryPredicate for GroupNameQuery<'a> {
+    type Item = Group;
+
+    fn matches(&self, other: &Self::Item) -> bool {
+        self.0 == &other.name
+    }
+}
+
+/// Provides facilities to build a new group chat 
+pub struct NewGroup<T: Storage<Item=Group>, S: Subscriptions> {
+    pub group_name: String,
+    pub peer_id: String,
+    pub storage: T,
+    pub subscriptions: S,
+    pub is_published: bool 
+}
+
+
+impl <T:Storage<Item=Group>, S: Subscriptions> NewGroup<T, S>{
+    ///  create a new group from the given data and save it 
+    /// to the storage instance provided
+    pub fn save(&mut self) -> Result<()>{
+        let group = Group::new(self.group_name.clone(), self.peer_id.clone());
+
+        self.storage.save(group)
+    } 
+
+    /// Subscribe to the new group 
+    /// Subscriptions always save the group first 
+    pub fn subscribe(&mut self) -> bool {
+        if !self.exists(){
+            // save it first 
+            self.save();
+        }
+
+        self.subscriptions.subscribe(&self.peer_id, &self.group_name)
+        
+    }
+
+    /// Check if a group has already been saved 
+    /// Creates a group name query predicate and issues a find on the 
+    /// storage. If the iterator returned is empty, then group is not created
+    fn exists(&self) -> bool {
+        let predicate = GroupNameQuery(&self.group_name);
+
+        self.storage.find(predicate).count() > 0
+    }
+
+}
