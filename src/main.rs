af06b853476322f2e337d7a20fe13b7b2ea1379c
ank3r <mwangishadrack5@gmail.com>
2021-01-05 01:54:52 +0300

    add working chat

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,124 @@
-extern crate peer_chat;
+use futures::prelude::*;
+use libp2p::{
+    Multiaddr, 
+    NetworkBehaviour, PeerId, Swarm, Transport, core::upgrade, floodsub::{self, Floodsub, FloodsubEvent}, 
+    identity, mdns::{Mdns, MdnsEvent}, mplex, noise, swarm::{NetworkBehaviourEventProcess, SwarmBuilder}, tcp::TokioTcpConfig};
+use std::error::Error;
+use tokio::io::{self, AsyncBufReadExt};
 use log::{info};
 
+// custom behaviour 
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    floodsub: Floodsub,
+    mdns: Mdns 
+}
 
-use peer_chat::{Peer, storage::MemoryStorage, Subscriber, Group,Publisher, Message};
-
-// stub subscriptions 
-struct DummySubscriptions;
-
-impl Subscriber for DummySubscriptions {
-    fn subscribe(&mut self, peer_id: &str, topic: &str) -> bool {
-        info!("{} subscribed to {}", peer_id, topic);
-
-        true 
-    }
-
-    fn unsubscribe(&mut self, peer_id: &str, topic: &str) -> bool {
-        info!("{} unsubscribed from {}", peer_id, topic);
-
-        true 
+impl NetworkBehaviourEventProcess<FloodsubEvent> for MyBehaviour {
+    // called when flood sub emits eventss 
+    fn inject_event(&mut self, message: FloodsubEvent){
+        if let FloodsubEvent::Message(msg) = message {
+            info!("Received: '{}' from '{}'", String::from_utf8_lossy(&msg.data), msg.source);
+        }
     }
 }
 
 
-// dummy publisher 
-struct DummyPublisher;
-
-impl Publisher<Message> for DummyPublisher {
-    fn publish(&mut self, msg: Message) {
-        info!("message {} published to {}", msg.id(), msg.group_name);
+impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
+    fn inject_event(&mut self, event: MdnsEvent){
+        match event {
+            MdnsEvent::Discovered(list) => {
+                for(peer, _) in list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            },
+            MdnsEvent::Expired(list) => {
+                for (peer, _) in list {
+                    self.floodsub.remove_node_from_partial_view(&peer);
+                }
+            }
+        }
     }
 }
 
-fn main() {
+
+/// main function 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
     pretty_env_logger::init();
-    // create two a peer 
-    let peer =  Peer::new("shaddyshad".to_string(), "ank3r".to_string());
 
-    // get the sub and memory storage 
-    let subscriptions = DummySubscriptions;
-    let storage: MemoryStorage<Group> = MemoryStorage::new();
+    // random keypair 
+    let id_keys = identity::Keypair::generate_ed25519();
+    let peer_id = PeerId::from_public_key(id_keys.public());
 
-    // create a new group 
-    let mut new_group = peer.new_group("chat001".to_string(), storage, subscriptions);
+    info!("local id {}", peer_id);
 
-    // save the group by subscribing 
-    new_group.subscribe();
+    // create a keypair for transport 
+    let noise_keys = noise::Keypair::<noise::X25519>::new()
+        .into_authentic(&id_keys)
+        .expect("signing libp2p-noise static DH keypair failed");
+
+    // tokio based tcpconnection using noise for authenticated transport and mplex streams
+    let transport = TokioTcpConfig::new().nodelay(true)
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::NoiseConfig::xx(noise_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    // flood sub topic 
+    let floodsub_topic = floodsub::Topic::new("chat");
+
+    let mut swarm = {
+        let mdns = Mdns::new()?;
+        let mut behaviour = MyBehaviour {
+            floodsub: Floodsub::new(peer_id.clone()),
+            mdns
+        };
+
+        behaviour.floodsub.subscribe(floodsub_topic.clone());   
+
+        SwarmBuilder::new(transport, behaviour, peer_id.clone())
+            // spawn background tasks into tokio rt
+            .executor(Box::new(|fut| {tokio::spawn(fut);}))
+            .build()
+    };
+
+    // reach out 
+    if let Some(to_dial) = std::env::args().nth(1){
+        let addr: Multiaddr = to_dial.parse()?;
+        Swarm::dial_addr(&mut swarm, addr)?;
+        info!("Dialed {:?}", to_dial);
+    }
 
 
-    // deps 
-    let publisher = DummyPublisher;
-    let msg_store: MemoryStorage<Message> = MemoryStorage::new();
-    // send amessage to the group 
-    let mut new_msg = peer.new_message("chat001", "message", msg_store, publisher);
-    new_msg.send();
+    // read lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
 
-    // finally, unsubscribe 
-    new_group.unsubscribe();
+    // listen 
+    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    
-    
+    // off 
+    let mut listening = false;
+    loop {
+        let to_publish = {
+            tokio::select! {
+                line = stdin.try_next() => Some((floodsub_topic.clone(), line?.expect("stdin closed"))),
+                event = swarm.next() => {
+                    info!("New event: {:?}", event);
+                    None 
+                }
+            }
+        };
+
+        if let Some((topic,line)) = to_publish {
+            swarm.floodsub.publish(topic, line.as_bytes());
+        }
+
+        if !listening {
+            for addr in Swarm::listeners(&swarm){
+                info!("listening on {:?}", addr);
+                listening = true;
+            }
+        }
+    }
 }
