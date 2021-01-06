1dc095e08c69028398b65fa866d389ebb1c28e9c
ank3r <mwangishadrack5@gmail.com>
2021-01-06 23:38:01 +0300

    update behaviour to include notifications

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,56 @@ use libp2p::{
     Multiaddr, 
     NetworkBehaviour, PeerId, Swarm, Transport, core::upgrade, floodsub::{self, Floodsub, FloodsubEvent}, 
     identity, mdns::{Mdns, MdnsEvent}, mplex, noise, swarm::{NetworkBehaviourEventProcess, SwarmBuilder}, tcp::TokioTcpConfig};
-use std::error::Error;
+use std::{error::Error, sync::{RwLock, Arc}};
 use tokio::io::{self, AsyncBufReadExt};
-use log::{info};
+use log::{info, warn};
+use peer_chat::{Message, Event, EventEmitter, Notifier, EventListener};
+
+
+/// Notifications 
+pub struct Notifications{
+    subject: String 
+}
+
+impl Notifications {
+    fn new(subject: String) -> Self {
+        Self {
+            subject
+        }
+    }
+}
+
+impl EventListener for Notifications {
+    fn handle(&mut self, evt: &Event) {
+        match evt {
+            Event::Msg(msg) => {
+                info!("{} -> {}", &msg.sender[0..5], msg.msg);
+            }
+        }
+    }
+
+    fn subject(&self) -> String {
+        self.subject.clone()
+    }
+}
 
 // custom behaviour 
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
     floodsub: Floodsub,
-    mdns: Mdns 
+    mdns: Mdns,
+    #[behaviour(ignore)]
+    notifier: Notifier<Notifications>
 }
 
 impl NetworkBehaviourEventProcess<FloodsubEvent> for MyBehaviour {
     // called when flood sub emits eventss 
     fn inject_event(&mut self, message: FloodsubEvent){
         if let FloodsubEvent::Message(msg) = message {
-            info!("Received: '{}' from '{}'", String::from_utf8_lossy(&msg.data), msg.source);
+            let msg_string = String::from_utf8_lossy(&msg.data);
+            let message: Message = serde_json::from_str(&msg_string).expect("can deserialize message");
+
+            self.notifier.emit(Event::Msg(message));
         }
     }
 }
@@ -51,7 +85,6 @@ async fn main() -> Result<(), Box<dyn Error>>{
     let id_keys = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from_public_key(id_keys.public());
 
-    info!("local id {}", peer_id);
 
     // create a keypair for transport 
     let noise_keys = noise::Keypair::<noise::X25519>::new()
@@ -68,11 +101,18 @@ async fn main() -> Result<(), Box<dyn Error>>{
     // flood sub topic 
     let floodsub_topic = floodsub::Topic::new("chat");
 
+    // create a notifications listeneer 
+    let nots = Arc::new(RwLock::new(Notifications::new("notifications".to_string())));
+
+    let mut emitter = Notifier::new();
+    emitter.subscribe(nots.clone());
+
     let mut swarm = {
         let mdns = Mdns::new()?;
         let mut behaviour = MyBehaviour {
             floodsub: Floodsub::new(peer_id.clone()),
-            mdns
+            mdns,
+            notifier: emitter
         };
 
         behaviour.floodsub.subscribe(floodsub_topic.clone());   
@@ -111,12 +151,16 @@ async fn main() -> Result<(), Box<dyn Error>>{
         };
 
         if let Some((topic,line)) = to_publish {
-            swarm.floodsub.publish(topic, line.as_bytes());
+            let message = Message::new(line, peer_id.to_string(), topic.id().to_string());
+
+            let message = serde_json::to_string(&message).expect("can parse into json");
+
+            swarm.floodsub.publish(topic, message.as_bytes());
         }
 
         if !listening {
             for addr in Swarm::listeners(&swarm){
-                info!("listening on {:?}", addr);
+                warn!("listening on {:?}", addr);
                 listening = true;
             }
         }
