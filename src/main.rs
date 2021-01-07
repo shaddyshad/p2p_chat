803c15cc115c76948f0d5128da219d538aa22a23
ank3r <mwangishadrack5@gmail.com>
2021-01-07 19:13:45 +0300

    adapt ui

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,11 @@
-use std::io;
-use tui::{
-    Terminal,
-    backend::TermionBackend,
-    widgets::{Block, Borders},
-    layout::{Layout, Constraint,Direction},
-};
-use termion::raw::IntoRawMode;
+use peer_chat::Ui;
 
 /// main function 
-fn main() -> Result<(), io::Error>{
+fn main(){
     pretty_env_logger::init();
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    
+    // create a new UI 
+    let mut ui = Ui::new();
 
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
-            )
-            .split(f.size());
-
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-
-        f.render_widget(block, chunks[0]);
-
-        let block = Block::default()
-            .title("Block 2")
-            .borders(Borders::ALL);
-
-        f.render_widget(block, chunks[1]);
-
-    })
+    ui.render().expect("can render ui");
 }
